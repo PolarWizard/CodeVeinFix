2d5defa6a0fced5ed904af38c2a4ae2935f89bdc
Dominik Protasewicz <dep9@njit.edu>
2024-07-21 21:57:01 -0400

    Initial commit
    	modified:   .gitignore
    	new file:   .gitmodules
    	new file:   CMakeLists.txt
    	modified:   LICENSE
    	modified:   README.md
    	new file:   install.ps1
    	new file:   main.cpp
    	new file:   utils.cpp
    	new file:   utils.hpp

---
+++ utils.cpp
@@ -0,0 +1,118 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Dominik Protasewicz
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <windows.h>
+#include <vector>
+#include <format>
+#include <iostream>
+
+#include "utils.hpp"
+
+namespace Utils
+{
+
+    std::string bytesToString(void* bytes, size_t size) {
+        std::string pattern;
+        for (size_t i = 0; i < size; i++) {
+            pattern += std::format("{:02X} ", ((uint8_t*)bytes)[i]);
+        }
+        if (!pattern.empty()) {
+            pattern.pop_back();
+        }
+        return pattern;
+    }
+
+    std::pair<int, int> GetDesktopDimensions() {
+        DEVMODE devMode{};
+        devMode.dmSize = sizeof(DEVMODE);
+        if (EnumDisplaySettings(nullptr, ENUM_CURRENT_SETTINGS, &devMode)) {
+            return { devMode.dmPelsWidth, devMode.dmPelsHeight };
+        }
+        return {};
+    }
+
+    void patch(uintptr_t address, const char* pattern)
+    {
+        static auto pattern_to_byte = [](const char* pattern) {
+            auto bytes = std::vector<uint8_t>{};
+            auto start = const_cast<char*>(pattern);
+            auto end = const_cast<char*>(pattern) + strlen(pattern);
+            for (auto current = start; current < end; ++current) {
+                bytes.push_back((uint8_t)strtoul(current, &current, 16));
+            }
+            return bytes;
+        };
+
+        DWORD oldProtect;
+        auto patternBytes = pattern_to_byte(pattern); 
+        VirtualProtect((LPVOID)address, patternBytes.size(), PAGE_EXECUTE_READWRITE, &oldProtect);
+        memcpy((LPVOID)address, patternBytes.data(), patternBytes.size());
+        VirtualProtect((LPVOID)address, patternBytes.size(), oldProtect, &oldProtect);
+    }
+
+    void patternScan(void* module, const char* signature, std::vector<uint64_t>* address)
+    {
+        static auto pattern_to_byte = [](const char* pattern) {
+            auto bytes = std::vector<int>{};
+            auto start = const_cast<char*>(pattern);
+            auto end = const_cast<char*>(pattern) + strlen(pattern);
+
+            for (auto current = start; current < end; ++current) {
+                if (*current == '?') {
+                    ++current;
+                    if (*current == '?')
+                        ++current;
+                    bytes.push_back(-1);
+                }
+                else {
+                    bytes.push_back(strtoul(current, &current, 16));
+                }
+            }
+            return bytes;
+        };
+
+        auto dosHeader = (PIMAGE_DOS_HEADER)module;
+        auto ntHeaders = (PIMAGE_NT_HEADERS)((std::uint8_t*)module + dosHeader->e_lfanew);
+
+        auto sizeOfImage = ntHeaders->OptionalHeader.SizeOfImage;
+        auto patternBytes = pattern_to_byte(signature);
+        auto scanBytes = reinterpret_cast<std::uint8_t*>(module);
+
+        auto s = patternBytes.size();
+        auto d = patternBytes.data();
+
+        for (auto i = 0ul; i < sizeOfImage - s; ++i) {
+            bool found = true;
+            for (auto j = 0ul; j < s; ++j) {
+                if (scanBytes[i + j] != d[j] && d[j] != -1) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                address->push_back((uint64_t)&scanBytes[i]);
+            }
+        }
+    }
+}
---
+++ utils.hpp
@@ -0,0 +1,91 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 Dominik Protasewicz
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <windows.h>
+#include <vector>
+#include <string>
+
+namespace Utils
+{
+    /**
+     * @brief Convert memory bytes into string representation
+     * @details Converts the bytes pointed to by the `bytes` parameter into a string.
+     *      The total number of bytes that will be converted is based on the `size`
+     *      parameter. The returned string will be in hexidecimal format and will be
+     *      organized as the bytes appear in memory. For example, if the `bytes` parameter
+     *      points to some integer in memory equal to `0x12345678`, and the `size` parameter
+     *      is given sizeof(int), then the returned string shall be "78 56 34 12", as that is
+     *      how `0x12345678` is stored in memory, on little endian x86_64.
+     * 
+     * @param bytes Pointer to memory
+     * @param size Size of `bytes` parameter
+     * @return std::string 
+     * 
+     * @code
+     * float a = 3.5555556; // In hex: 0x40638E39
+     * std::string string = bytesToString(&a, sizeof(float));
+     * std::cout << string << std::endl; // Prints "39 8E 63 40"
+     * @endcode
+     */
+    std::string bytesToString(void* bytes, size_t size);
+
+    /**
+     * @brief Get the width and height, respectively, of the desktop in pixels
+     * 
+     * @return std::pair<int, int> 
+     */
+    std::pair<int, int> GetDesktopDimensions();
+
+    /**
+     * @brief Patch an area of memory with a pattern
+     * @details Patches an area of memory pointed to by `address` with the pattern.
+     *      The `pattern` parameter is expected to be in the form of a hex string as such:
+     *      "DE AD BE EF". There is no limit is how long the pattern string can be, but
+     *      it is important to be mindful as to not go out of bounds of the program space
+     *      else a segmentation fault will occur. The total amount of memory that will
+     *      be patched is determined by the size of the `pattern` parameter. If the example
+     *      hex string above is referenced then the total amount of memory that will be
+     *      patched is 4 bytes, starting at the address[0] and ending at the address[3].
+     * 
+     * @param address Starting memory address
+     * @param pattern IDA-style byte array pattern
+     */
+    void patch(uintptr_t address, const char* pattern);
+
+    /**
+     * @brief Scan for a given byte pattern on a module
+     * @details Obtained and modified from:
+     *      https://github.com/OneshotGH/CSGOSimple-master/blob/master/CSGOSimple/helpers/utils.cpp
+     *      Original implementation is for the most part intact. Modified so that all
+     *      the addresses where the pattern is found is appended to the `address` vector,
+     *      instead of returning the address when the first instance is found.
+     *      
+     * @param module Base of the module to search
+     * @param signature IDA-style byte array pattern
+     * @param address Vector of addresses where the pattern was found
+     */
+    void patternScan(void* module, const char* signature, std::vector<uint64_t>* address);
+}
